35753a12e0b0e42e55ac3d7dd66752490e79bce5
Me <gekoncze@seznam.cz>
2021-12-05 13:45:58 +0100

    ... bug fix

--- src/cz/mg/entity/Entities.java
+++ src/cz/mg/entity/Entities.java
@@ -8,7 +8,7 @@ import cz.mg.annotations.requirement.Optional;
 public @Utility class Entities {
     public static boolean isEntity(@Optional Object object){
         if(object != null){
-            return object.getClass().isAssignableFrom(Entity.class);
+            return object.getClass().isAnnotationPresent(Entity.class);
         } else {
             return false;
         }
