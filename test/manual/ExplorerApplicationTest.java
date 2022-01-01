5d6a777cfdf3632f6b37306694974708a591abc1
Me <gekoncze@seznam.cz>
2022-01-01 13:16:23 +0100

    added simple icons for new test classes

--- test/manual/ExplorerApplicationTest.java
+++ test/manual/ExplorerApplicationTest.java
@@ -1,12 +1,14 @@
 package manual;
 
 import cz.mg.entity.explorer.gui.components.ExplorerWindow;
+import cz.mg.entity.explorer.gui.services.GalleryIconService;
 import cz.mg.test.Test;
 import cz.mg.test.annotations.TestCase;
 import cz.mg.test.cli.runners.SingleTestClassRunner;
 import utilities.TestInitializer;
 import utilities.TestMapper;
 import utilities.TestRoot;
+import utilities.icons.Icons;
 
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -24,6 +26,7 @@ public class ExplorerApplicationTest implements Test {
             new TestMapper(),
             new FileNameExtensionFilter("Test files (*.test)", "test")
         );
+        new GalleryIconService().initialize(window.getGallery(), Icons.class);
         window.setVisible(true);
     }
 }
---
+++ test/utilities/icons/Icons.java
@@ -0,0 +1,11 @@
+package utilities.icons;
+
+import cz.mg.annotations.classes.Utility;
+
+
+public @Utility class Icons {
+    public static final String TEST_BRANCH = "testbranch.png";
+    public static final String TEST_ENUM = "testenum.png";
+    public static final String TEST_LEAF = "testleaf.png";
+    public static final String TEST_ROOT = "testroot.png";
+}
